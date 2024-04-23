4d33cf1fbe2e605ba1134499cd04ce8760b4b072
tien <thuytien16102003@gmail.com>
2024-04-23 14:08:22 +0700

    update sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -32,7 +32,7 @@
             </a>
         </li>
         <li>
-            <a href="#" class="nav-link text-white">
+            <a href="{{ route('admin.books.index') }}" class="nav-link text-white">
                 <svg class="bi me-2" width="16" height="16">
                     <use xlink:href="#grid" />
                 </svg>
