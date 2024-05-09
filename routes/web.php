c0210128f639aaab181714b1afa138c51b0043cb
qui <ngocquivo11@gmail.com>
2024-05-10 00:41:38 +0700

    change location route

--- routes/web.php
+++ routes/web.php
@@ -19,46 +19,6 @@ use App\Notifications\BookPartPublished;
 
 
 Route::group(['namespace' => 'App\Http\Controllers'], function () {
-    // Categories
-    Route::get('/admin/categories', 'CategoryController@index')->name('admin.categories.index');
-    Route::get('/admin/categories/add', 'CategoryController@create')->name('admin.categories.create');
-    Route::post('/admin/categories/add', 'CategoryController@add')->name('admin.categories.add');
-    Route::get('/admin/categories/edit/{id}', 'CategoryController@edit')->name('admin.categories.edit');
-    Route::post('/admin/categories/update', 'CategoryController@update')->name('admin.categories.update');
-    Route::get('/admin/categories/delete/{id}', 'CategoryController@delete')->name('admin.categories.delete');
-
-    // Authors
-    Route::get('/admin/authors', 'AuthorController@index')->name('admin.authors.index');
-    Route::get('/admin/authors/create', 'AuthorController@create')->name('admin.authors.create');
-    Route::post('/admin/authors/store', 'AuthorController@store')->name('admin.authors.store');
-    Route::get('/admin/authors/delete/{id}', 'AuthorController@delete')->name('admin.authors.delete');
-    Route::get('/admin/authors/edit/{id}', 'AuthorController@edit')->name('admin.authors.edit');
-    Route::post('/admin/authors/update', 'AuthorController@update')->name('admin.authors.update');
-
-    // Books
-    Route::get('/admin/books', 'BookController@index')->name('admin.books.index');
-    Route::get('/admin/books/create', 'BookController@create')->name('admin.books.create');
-    Route::post('/admin/books/store', 'BookController@store')->name('admin.books.store');
-    Route::get('/admin/books/edit/{id}', 'BookController@edit')->name('admin.books.edit');
-    Route::post('/admin/books/update', 'BookController@update')->name('admin.books.update');
-    Route::post('/admin/books/delete/{id}', 'BookController@delete')->name('admin.books.delete');
-
-    // Bookparts
-    Route::get('/admin/books/{id}/bookparts/create', 'BookPartController@create')->name('admin.bookparts.create');
-    Route::post('/admin/books/bookparts/store', 'BookPartController@store')->name('admin.bookparts.store');
-    Route::get('/admin/books/bookparts/edit/{id}', 'BookPartController@edit')->name('admin.bookparts.edit');
-    Route::post('/admin/books/bookparts/update', 'BookPartController@update')->name('admin.bookparts.update');
-    Route::get('/admin/books/bookparts/delete/{id}', 'BookPartController@delete')->name('admin.bookparts.delete');
-
-
-    // Users
-    Route::get('/admin/users', 'UserController@index')->name('admin.users.index');
-    Route::get('/admin/users/create', 'UserController@create')->name('admin.users.create');
-    Route::post('/admin/users/store', 'UserController@store')->name('admin.users.store');
-    Route::get('/admin/users/edit/{id}', 'UserController@edit')->name('admin.users.edit');
-    Route::post('/admin/users/update', 'UserController@update')->name('admin.users.update');
-    Route::post('/admin/users/delete/{id}', 'UserController@update')->name('admin.users.delete');
-
 
     //search
     Route::get('/', 'HomeController@index')->name('home.index');
@@ -92,6 +52,46 @@ Route::group(['namespace' => 'App\Http\Controllers'], function () {
          */
         Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
         Route::post('/comment', 'HomeController@review')->name('home.review');
+
+        // Categories
+        Route::get('/admin/categories', 'CategoryController@index')->name('admin.categories.index');
+        Route::get('/admin/categories/add', 'CategoryController@create')->name('admin.categories.create');
+        Route::post('/admin/categories/add', 'CategoryController@add')->name('admin.categories.add');
+        Route::get('/admin/categories/edit/{id}', 'CategoryController@edit')->name('admin.categories.edit');
+        Route::post('/admin/categories/update', 'CategoryController@update')->name('admin.categories.update');
+        Route::get('/admin/categories/delete/{id}', 'CategoryController@delete')->name('admin.categories.delete');
+
+        // Authors
+        Route::get('/admin/authors', 'AuthorController@index')->name('admin.authors.index');
+        Route::get('/admin/authors/create', 'AuthorController@create')->name('admin.authors.create');
+        Route::post('/admin/authors/store', 'AuthorController@store')->name('admin.authors.store');
+        Route::get('/admin/authors/delete/{id}', 'AuthorController@delete')->name('admin.authors.delete');
+        Route::get('/admin/authors/edit/{id}', 'AuthorController@edit')->name('admin.authors.edit');
+        Route::post('/admin/authors/update', 'AuthorController@update')->name('admin.authors.update');
+
+        // Books
+        Route::get('/admin/books', 'BookController@index')->name('admin.books.index');
+        Route::get('/admin/books/create', 'BookController@create')->name('admin.books.create');
+        Route::post('/admin/books/store', 'BookController@store')->name('admin.books.store');
+        Route::get('/admin/books/edit/{id}', 'BookController@edit')->name('admin.books.edit');
+        Route::post('/admin/books/update', 'BookController@update')->name('admin.books.update');
+        Route::post('/admin/books/delete/{id}', 'BookController@delete')->name('admin.books.delete');
+
+        // Bookparts
+        Route::get('/admin/books/{id}/bookparts/create', 'BookPartController@create')->name('admin.bookparts.create');
+        Route::post('/admin/books/bookparts/store', 'BookPartController@store')->name('admin.bookparts.store');
+        Route::get('/admin/books/bookparts/edit/{id}', 'BookPartController@edit')->name('admin.bookparts.edit');
+        Route::post('/admin/books/bookparts/update', 'BookPartController@update')->name('admin.bookparts.update');
+        Route::get('/admin/books/bookparts/delete/{id}', 'BookPartController@delete')->name('admin.bookparts.delete');
+
+
+        // Users
+        Route::get('/admin/users', 'UserController@index')->name('admin.users.index');
+        Route::get('/admin/users/create', 'UserController@create')->name('admin.users.create');
+        Route::post('/admin/users/store', 'UserController@store')->name('admin.users.store');
+        Route::get('/admin/users/edit/{id}', 'UserController@edit')->name('admin.users.edit');
+        Route::post('/admin/users/update', 'UserController@update')->name('admin.users.update');
+        Route::post('/admin/users/delete/{id}', 'UserController@update')->name('admin.users.delete');
 
 
         // cart
